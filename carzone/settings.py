111d375951175082b2af8596b24279077b53882c
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2020-12-04 10:19:45 +0100

    settings

--- carzone/settings.py
+++ carzone/settings.py
@@ -91,8 +91,10 @@ DATABASES = {
     }
 }
 
-DATABASES = {'default': dj_database_url.config(default='postgres://postgres:pythondeveloper2019@localhost/carzone_db')}
+# DATABASES = {'default': dj_database_url.config(default='postgres://postgres:pythondeveloper2019@localhost/carzone_db')}
 
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
