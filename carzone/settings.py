f049e9a7946e49f685715aeaaff5f74ad9d8a2f9
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2020-12-04 10:18:08 +0100

    settings

--- carzone/settings.py
+++ carzone/settings.py
@@ -80,16 +80,16 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'carzone_db',
-#         'USER': 'postgres',
-#         'PASSWORD': 'pythondeveloper2019',
-#         'HOST': 'localhost',
-#         'PORT': '5432'
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carzone_db',
+        'USER': 'postgres',
+        'PASSWORD': 'pythondeveloper2019',
+        'HOST': 'localhost',
+        'PORT': '5432'
+    }
+}
 
 DATABASES = {'default': dj_database_url.config(default='postgres://postgres:pythondeveloper2019@localhost/carzone_db')}
 
