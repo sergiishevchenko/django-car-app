2e82d428c9d07e37864d913fa62a5fec74100e03
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2020-12-04 10:10:26 +0100

    settings

--- carzone/settings.py
+++ carzone/settings.py
@@ -1,5 +1,7 @@
 import os
 import dj_database_url
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -155,3 +157,5 @@ EMAIL_USE_TLS = True
 
 # whitenoise
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+django_heroku.settings(locals())
